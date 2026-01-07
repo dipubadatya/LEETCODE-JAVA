9f1ddb88b466c83523c113303e6f5238e8a923f9
dipubadatya <148934509+dipubadatya@users.noreply.github.com>
2026-01-07 12:06:37 +0530

    Time: 1 ms (23.33%) | Memory: 42.8 MB (83.80%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c:  s.toCharArray()){
+            if(c >= 65 && c <= 90){
+                c= (char) (c + 32);
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
