ea3f8d3e53d4b5d6c6d0c274c9523d0747e59eb2
dipubadatya <148934509+dipubadatya@users.noreply.github.com>
2026-01-20 21:49:46 +0530

    Time: 2 ms (96.44%) | Memory: 48.5 MB (88.38%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] countBits(int n) {
+
+        int[] result = new int[n + 1];
+
+        if (n == 0)
+            return result;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                result[i] = result[i / 2] + 1;
+            } else {
+                result[i] = result[i / 2];
+            }
+        }
+        return result;
+    }
+}
