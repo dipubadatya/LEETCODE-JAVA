25902d9a74ae30d76197309c7f2538a2a28d924e
DIPU BADATYA <148934509+dipubadatya@users.noreply.github.com>
2026-03-11 23:09:50 +0530

    Time: 4 ms (22.18%) | Memory: 65.7 MB (11.08%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+
+    let maxSum = nums[0];
+    let currentSum = 0;
+
+    for(let num of nums){
+
+        if(currentSum < 0){
+            currentSum = 0;
+        }
+
+        currentSum += num;
+
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+};
