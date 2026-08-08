b2679ec1dca31751000e55f2a2588335741a50bb
DIPU BADATYA <148934509+dipubadatya@users.noreply.github.com>
2026-08-08 10:08:29 +0530

    Time: 211 ms (23.74%), Space: 92.2 MB (24.83%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} target
+ * @param {number[]} position
+ * @param {number[]} speed
+ * @return {number}
+ */
+var carFleet = function(target, position, speed) {
+    const cars = [];
+
+    for (let i = 0; i < position.length; i++) {
+        cars.push([position[i], speed[i]]);
+    }
+
+    cars.sort((a, b) => b[0] - a[0]);
+
+    const stack = [];
+
+    for (const [pos, spd] of cars) {
+        const time = (target - pos) / spd;
+
+        if (stack.length === 0 || time > stack[stack.length - 1]) {
+            stack.push(time);
+        }
+    }
+
+    return stack.length;
+};
