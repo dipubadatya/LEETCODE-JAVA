6fccce55ce2fbf9eb4388a7aabef517363a837e2
DIPU BADATYA <148934509+dipubadatya@users.noreply.github.com>
2026-08-09 19:39:50 +0530

    Time: 0 ms (100%), Space: 53.1 MB (87.56%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,29 @@
+var searchMatrix = function(matrix, target) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    let left = 0;
+    let right = m * n - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+  
+        const row = Math.floor(mid / n);
+        const col = mid % n;
+
+        const value = matrix[row][col];
+
+        if (value === target) {
+            return true;
+        }
+
+        if (value < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+};
