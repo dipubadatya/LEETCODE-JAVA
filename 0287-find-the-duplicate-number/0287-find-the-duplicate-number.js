677e48e2890f48f43c700d83587c452e3d5bf666
DIPU BADATYA <148934509+dipubadatya@users.noreply.github.com>
2026-08-20 08:36:12 +0530

    Time: 20 ms (34.25%), Space: 76.9 MB (17.73%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+
+var findDuplicate = function(nums) {
+    let set = new Set();
+
+    for (let num of nums) {
+        if (set.has(num)) {
+            return num;
+        }
+
+        set.add(num);
+    }
+};
